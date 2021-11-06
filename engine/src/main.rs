b60d38bad59637e1f0444a6e959293f7745df3f8
kevin <kevinchannon@gmail.com>
2021-11-06 09:08:59 +0000

    Adding a command line interface and app details

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,3 +1,18 @@
+use clap::{Arg, App};
+use std::println;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Run calculations on various renewables scenarios for the house")
+        .version("0.0.1")
+        .author("Kevin Channon")
+        .about("Fill me in later!")
+        .arg(Arg::with_name("config")
+            .short("c")
+            .long("config")
+            .takes_value(true)
+            .help("Path to the config file for the app"))
+        .get_matches();
+
+    let config_file = matches.value_of("config").unwrap_or("default.json");
+    println!("Using config from {}", config_file);
 }
