a1805e843579f2059028f81ae888b1fa73e48db7
kevin <kevinchannon@gmail.com>
2021-11-13 08:32:00 +0000

    Calculating run time and step time increments

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -3,16 +3,93 @@ use std::println;
 use std::fs;
 use serde::{Serialize, Deserialize};
 use serde_json;
+use chrono::{Utc, Duration, DateTime};
+use std::mem;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Position {
+    latitude: f32,
+    longitude: f32,
+    elevation: f32
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Orientation {
+    direction: f32,
+    slope: f32
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct SolarPanelArray {
+    size: u32,
+    position: Position,
+    orientation: Orientation,
+    nominal_power_w: f32
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+enum Supply {
+    Solar { panels: std::vec::Vec<SolarPanelArray> },
+    Wind {},
+    Grid {}
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Load {
+
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Storage {
+
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Configuration {
-    run_length_days: u64
+    run_length_days: u64,
+    supplies: std::vec::Vec<Supply>,
+    loads: std::vec::Vec<Load>,
+    stores: std::vec::Vec<Storage>
+}
+
+/// Evaluate the state of the simulation at the specified time point.
+fn evaluate_at_time_point(time_point: DateTime<Utc>, cfg: &Configuration) {
+    println!("Evaluating at {:?}", time_point);
+}
+
+struct DateTimeIterator(DateTime<Utc>, DateTime<Utc>, Duration);
+
+impl Iterator for DateTimeIterator {
+
+    type Item = DateTime<Utc>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.0 <= self.1 {
+            let next_item = self.0 + self.2;
+            return Some(mem::replace(&mut self.0, next_item));
+        } else {
+            return None;
+        }
+    }
 }
 
 /// Evaluate everything.
 fn evaluate(cfg: Configuration) -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Run length = {} days", cfg.run_length_days);
+
+    let start = Utc::now();
+    let step = Duration::minutes(2);
+    let end = start + Duration::days(cfg.run_length_days as i64);
+
+    let total_steps = (end - start).num_minutes() / step.num_minutes();
+
+    let mut time_point = start;
+    for t in DateTimeIterator(start, end, step) {
+        evaluate_at_time_point(t, &cfg);
+        
+        time_point = time_point + step;
+    }
 
     return Ok(());
 }
